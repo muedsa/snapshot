59b979137ccd24bec2f4048b28ce2a16d7ecc37a
MUEDSA <7676275+muedsa@users.noreply.github.com>
2024-01-23 14:23:51 +0800

    update: remove benchmark from test

--- src/test/kotlin/com/muedsa/snapshot/benchmark/LogoBenchmark.kt
+++ src/test/kotlin/com/muedsa/snapshot/benchmark/LogoBenchmark.kt
@@ -3,13 +3,16 @@ package com.muedsa.snapshot.benchmark
 import com.muedsa.snapshot.LogoCreator.Companion.logoContent
 import com.muedsa.snapshot.SnapshotImage
 import com.muedsa.snapshot.drawWidget
-import kotlin.test.Test
+import com.muedsa.snapshot.rendering.box.BoxConstraints
+import com.muedsa.snapshot.widget.ProxyWidget
 
 
 class LogoBenchmark {
 
-    @Test
-    fun benchmark() {
+    //@Test
+    fun image_benchmark() {
+        println("\n\nimage_benchmark")
+        System.gc()
         val count = 10000
         val startTime = System.currentTimeMillis()
         for (i in 0..count) {
@@ -19,9 +22,45 @@ class LogoBenchmark {
         }
         val totalTime = System.currentTimeMillis() - startTime
         val info = "Count: $count\nToto milliseconds: $totalTime\nAverage milliseconds: ${totalTime.toFloat() / count}"
+        println(info)
         drawWidget("benchmark/logo", debugInfo = info) {
             logoContent()
         }
+    }
+
+    //@Test
+    fun renderTree_benchmark() {
+        println("\n\nrenderTree_benchmark")
+        System.gc()
+        val widget = ProxyWidget().apply {
+            logoContent()
+        }
+        val count = 1000000
+        val startTime = System.currentTimeMillis()
+        for (i in 0..count) {
+            widget.createRenderBox()
+        }
+        val totalTime = System.currentTimeMillis() - startTime
+        val info = "Count: $count\nToto milliseconds: $totalTime\nAverage milliseconds: ${totalTime.toFloat() / count}"
+        println(info)
+    }
+
+    //@Test
+    fun layout_benchmark() {
+        println("\n\nlayout_benchmark")
+        System.gc()
+        val widget = ProxyWidget().apply {
+            logoContent()
+        }
+        val renderRoot = widget.createRenderBox()
+        val count = 1000000
+        val startTime = System.currentTimeMillis()
+        for (i in 0..count) {
+            renderRoot.layout(BoxConstraints())
+        }
+        val totalTime = System.currentTimeMillis() - startTime
+        val info = "Count: $count\nToto milliseconds: $totalTime\nAverage milliseconds: ${totalTime.toFloat() / count}"
+        println(info)
     }
 
 }
